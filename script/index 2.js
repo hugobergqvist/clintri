394b6a9c415873e38b4d2896c3972652157067f3
davidmagnussson <davidmagnusson97@gmail.com>
2020-02-16 00:03:11 +0100

    Fixade lite till

---
+++ script/index 2.js
@@ -0,0 +1,33 @@
+
+// Handle the search
+searchfunction = (e) => {
+    e.preventDefault(); // Prevents the form to submit, it catches the form before it is sent
+}
+
+addTreeMap = () => {
+    // Add new treemap
+    var newTreemap = document.createElement("div");
+    newTreemap.classList.add("treemap");
+    // ADD TREEMAP: newTreemap.appendChild(treemap);
+    var newContent = document.createTextNode("Hi there and greetings!");
+    newTreemap.appendChild(newContent);
+
+    // Add Legend
+    var newLegend = document.createElement("LEGEND");
+    var legendText = document.createTextNode("NewLegend:");  // Make dynamic with incoming data!!
+    newLegend.appendChild(legendText);
+
+    // Add Fieldset
+    var newFieldset = document.createElement("fieldset");
+    newFieldset.appendChild(newLegend);
+    newFieldset.appendChild(newTreemap);
+
+    // Add entire new div with all its children
+    var newDiv = document.createElement("div");
+    newDiv.classList.add("categories");
+    newDiv.appendChild(newFieldset);
+
+    // Find the wrapper containing all categories and push the new element
+    var wrapperDiv = document.getElementById("categorieContainer");
+    wrapperDiv.appendChild(newDiv);
+}
