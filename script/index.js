2ac009d203e1773b65f9f20fbff4e49c784ed667
Oliver Shin <oliver.shin.swe@gmail.com>
2020-02-16 18:27:47 +0100

    treemap is now interactive

--- script/index.js
+++ script/index.js
@@ -1,17 +1,36 @@
 
+//Load data
+loadJSON = () => {
+
+  return new Promise(function (resolve, reject) {
+
+    d3.json("./script/data.json", function (data) {
+      console.log("LOADING...")
+
+      resolve(data)
+
+       });
+   
+  })
+}
 
 // Handle the search
 searchfunction = (e) => {
 
 }
 
-addTreeMap = () => {
+
+
+addTreeMap = (data) => {
+
+    console.log("addtreemap")
 
     // Add new treemap
-    var newTreemap = document.createElement("div");
-    newTreemap.classList.add("treemap");
-    newTreemap.setAttribute("id", "treemap_cancer");
-    buildTreeMap()
+    buildTreeMap(data)
+    //var newTreemap = document.createElement("div");
+    //newTreemap.classList.add("treemap");
+    //newTreemap.setAttribute("id", "treemap");
+    
 
     // ADD TREEMAP: newTreemap.appendChild(treemap);
     var newContent = document.createTextNode("Hi there and greetings!");
@@ -37,9 +56,7 @@ addTreeMap = () => {
     wrapperDiv.appendChild(newDiv);
 }
 
-buildTreeMap = () => {
-
-    console.log("hej")
+buildTreeMap = (data) => {
 
     // set the dimensions and margins of the graph
 var margin = {top: 10, right: 10, bottom: 10, left: 10},
@@ -47,7 +64,7 @@ width = 1600 - margin.left - margin.right,
 height = 1000 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
-var svg = d3.select("#treemap_cancer")
+var svg = d3.select("#categorieContainer")
 .append("svg")
 .attr("width", width + margin.left + margin.right)
 .attr("height", height + margin.top + margin.bottom)
@@ -56,16 +73,55 @@ var svg = d3.select("#treemap_cancer")
       "translate(" + margin.left + "," + margin.top + ")");
 
 // read json data
-d3.json("./script/data.json", function(data) {
+console.log("reading")
 console.log(data)
 // Give the data to this cluster layout:
 var root = d3.hierarchy(data).sum(function(d){ return d.value}) // Here the size of each leave is given in the 'value' field in input data
+console.log(root.leaves())
 
 // Then d3.treemap computes the position of each element of the hierarchy
 d3.treemap()
   .size([width, height])
   .padding(2)
   (root)
+
+
+//Mouseover transitions
+let mouseOver = function (d) {
+  d3.selectAll(".leaf")
+    .transition()
+    .duration(200)
+    .style("opacity", .5)
+  d3.select(this)
+    .transition()
+    .duration(200)
+    .style("opacity", 1)
+
+}
+
+let mouseLeave = function (d) {
+  d3.selectAll(".leaf")
+    .transition()
+    .duration(200)
+    .style("opacity", .8)
+  d3.select(this)
+    .transition()
+    .duration(200)
+    .style("stroke", "transparent")
+
+}
+
+let mouseClick = function (d) {
+  d3.select(this)
+    .transition()
+    .duration(70)
+    .style("opacity", 1)
+    .style("stroke", "black")
+    .style("stroke-width", "5")
+    .transition()
+    .duration(70)
+    .style("stroke", "transparent")
+}
 
 // use this information to add rectangles:
 svg
@@ -77,8 +133,12 @@ svg
     .attr('y', function (d) { return d.y0; })
     .attr('width', function (d) { return d.x1 - d.x0; })
     .attr('height', function (d) { return d.y1 - d.y0; })
+    .attr('class', "leaf")
     .style("stroke", "black")
     .style("fill", "slateblue")
+    .on("mouseover", mouseOver)
+    .on("mouseleave", mouseLeave)
+    .on("click", mouseClick)
 
 // and to add the text labels
 svg
@@ -91,7 +151,12 @@ svg
     .text(function(d){ return d.data.name })
     .attr("font-size", "15px")
     .attr("fill", "white")
-})
 
 }
+
+main = () => {
+  loadJSON().then( function(JSON_data) {addTreeMap(JSON_data)} )
+}
+
+main()
 
