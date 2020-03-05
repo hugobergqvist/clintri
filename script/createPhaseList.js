c82d5175df4bb1cd49412cc6656a0b1097e1f24e
hugobergqvist <hugo@bergqvist.com>
2020-03-05 14:32:17 +0100

    added list on phase click

---
+++ script/createPhaseList.js
@@ -0,0 +1,89 @@
+const createPhaseList = lists => {
+  // Remove previous component
+  let sankey = document.getElementById("sankeyContainer");
+  sankey.style.display = "none";
+  let phaseListContainer = document.getElementById("phaseContentListContainer");
+
+  createListNavigation();
+
+  let table = document.createElement("table");
+  table.setAttribute("id", "phaseTable");
+
+  let tableHead = document.createElement("thead");
+  tableHead.setAttribute("id", "phaseTableHeader");
+
+  let headRow = tableHead.insertRow();
+  let titleHeaderCell = headRow.insertCell(0);
+  let dateHeaderCell = headRow.insertCell(1);
+  let titleHeader = document.createTextNode("Study title");
+  let dateHeader = document.createTextNode("Start date");
+  titleHeaderCell.appendChild(titleHeader);
+  dateHeaderCell.appendChild(dateHeader);
+
+  let tableBody = document.createElement("tbody");
+  tableBody.setAttribute("id", "phaseTableBody");
+
+  lists.forEach(element => {
+    let newRow = tableBody.insertRow();
+    let titleCell = newRow.insertCell(0);
+    let dateCell = newRow.insertCell(1);
+
+    let title = document.createTextNode(element["BriefTitle"]);
+    let date = document.createTextNode(element["StartDate"]);
+
+    titleCell.appendChild(title);
+    dateCell.appendChild(date);
+  });
+
+  table.appendChild(tableHead);
+  table.appendChild(tableBody);
+  phaseListContainer.appendChild(table);
+
+  console.log(lists);
+};
+
+const createListNavigation = () => {
+  const existing_element = document.querySelector("#sankeyContainer");
+  var searchBar = document.createElement("INPUT");
+  searchBar.setAttribute("type", "text");
+  searchBar.placeholder = "Ex: Heart";
+  existing_element.appendChild(searchBar);
+};
+
+const alphabethicalSort = arr => {
+  // sort() returns the sorted array.
+  arr.sort(function(a, b) {
+    var titleA = a.BriefTitle;
+    var titleB = b.BriefTitle;
+    if (titleA < titleB) {
+      //If compareFunction(a, b) returns less than 0, sort a to an index lower than b (i.e. a comes first).
+      return -1;
+    }
+    if (titleA > titleB) {
+      //If compareFunction(a, b) returns greater than 0, sort b to an index lower than a (i.e. b comes first).
+      return 1;
+    }
+    //If compareFunction(a, b) returns 0, leave a and b unchanged with respect to each other, but sorted with respect to all different elements.
+    return 0;
+  });
+
+  return arr;
+};
+
+const dateSort = arr => {
+  arr.sort(function(a, b) {
+    return new Date(b.StartDate) - new Date(a.StartDate);
+  });
+  return arr;
+};
+
+const stringSearch = (arr, keyword) => {
+  newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].BriefTitle[0].includes(keyword)) {
+      //check if the current study title have the keywords
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+};
--- script/sankeyTree.js
+++ script/sankeyTree.js
@@ -1,6 +1,6 @@
 buildSankeyTree = (data, phaseData) => {
   return (
-    new Promise(function (resolve, reject) {
+    new Promise(function(resolve, reject) {
       // set the dimensions and margins of the graph
       var sankeyDiv = document.getElementById("sankeyContainer");
       var divWidth = sankeyDiv.offsetWidth;
@@ -9,11 +9,11 @@ buildSankeyTree = (data, phaseData) => {
         divHeight = 500;
       }
       var margin = {
-        top: 10,
-        right: 10,
-        bottom: 10,
-        left: 10
-      },
+          top: 10,
+          right: 10,
+          bottom: 10,
+          left: 10
+        },
         width = divWidth - margin.left - margin.right, // Kanske går att lägga inherit här istället men det påverkar möjligheten att visa upp allt
         height = divHeight - margin.top - margin.bottom;
 
@@ -43,85 +43,11 @@ buildSankeyTree = (data, phaseData) => {
         .nodePadding(50)
         .size([width, height]);
 
-      console.log("phaseData in sankeyTree: ", phaseData);
-
-      let phase4 = phaseData["Phase 4"];
-      let phase41 = [...phase4];
-      let phase42 = [...phase4];
-      let phase43 = [...phase4];
-
-      console.log("this is phase 4:", phase4);
-
-      alphabethicalSort = arr => {
-        // sort() returns the sorted array.
-        arr.sort(function (a, b) {
-          var titleA = a.BriefTitle;
-          var titleB = b.BriefTitle;
-          if (titleA < titleB) {
-            //If compareFunction(a, b) returns less than 0, sort a to an index lower than b (i.e. a comes first).
-            return -1;
-          }
-          if (titleA > titleB) {
-            //If compareFunction(a, b) returns greater than 0, sort b to an index lower than a (i.e. b comes first).
-            return 1;
-          }
-          //If compareFunction(a, b) returns 0, leave a and b unchanged with respect to each other, but sorted with respect to all different elements.
-          return 0;
-        });
-
-        return arr;
-      };
-
-      console.log("this is sorted Alphabetically: ", alphabethicalSort(phase41));
-      //console.log("this is sorted Alphabetically reverse: ", alphabethicalSort(phase4).reverse());
-
-      dateSort = arr => {
-        arr.sort(function (a, b) {
-          return new Date(b.StartDate) - new Date(a.StartDate);
-        });
-        return arr;
-      };
-
-      console.log("this is sorted by Date", dateSort(phase42));
-
-      stringSearch = (arr, keyword) => {
-        newArr = [];
-        for (let i = 0; i < arr.length; i++) {
-          if (arr[i].BriefTitle[0].includes(keyword)) {
-            //check if the current study title have the keywords
-            newArr.push(arr[i]);
-          }
-        }
-        return newArr;
-      };
-
-      console.log("This is String Search with keyword 'Pretreatment'", stringSearch(phase43, "Pretreatment"));
-
-      // This updates the breadcrumbnavigation, sankey-breadcrumb
-      const condition = getCondition();
-      document.getElementById("breadcrumbSankey").innerHTML = condition;
-      const breadcrumbSankey = document.getElementById("breadcrumbSankey");
-      breadcrumbSankey.classList.remove("hideBreadcrumb");
-
-      const breadcrumbList = document.getElementById("breadcrumbList");
-      const breadcrumbStudy = document.getElementById("breadcrumbStudy");
-
-      if (!breadcrumbList.classList.contains("hideBreadcrumb")) {
-        breadcrumbList.classList.add("hideBreadcrumb");
-      } else if (!breadcrumbStudy.classList.contains("hideBreadcrumb")) {
-        breadcrumbStudy.classList.add("hideBreadcrumb");
-      }
-
-
-      //console.log("data in sankeyTree.js: ", data);
-
       // Constructs a new Sankey generator with the default settings.
       sankey
         .nodes(data.nodes)
         .links(data.links)
         .layout(0);
-
-
 
       //Phase content list view builder
       function buildPhaseContentList(phaseData) {
@@ -132,31 +58,28 @@ buildSankeyTree = (data, phaseData) => {
         let html = "";
 
         for (i = 0; i < phaseData.length; i++) {
-
-          let div = "<div>" + phaseData[i].BriefTitle[0] + "</div>"
+          let div = "<div>" + phaseData[i].BriefTitle[0] + "</div>";
           html += div;
         }
         phaseList.innerHTML = html;
-
       }
 
       //Mouse action functions
-      let mouseClick = function () {
-        console.log("click")
+      let mouseClick = function() {
+        console.log("click");
         let phase = d3.select(this).attr("phase");
 
         if (phase != "Interventional" && phase != "Observational") {
-          console.log(phase)
-          console.log(phaseData[phase])
+          console.log(phase);
+          console.log(phaseData[phase]);
           let currentPhaseData = phaseData[phase];
-          buildPhaseContentList(currentPhaseData)
+          buildPhaseContentList(currentPhaseData);
         }
+      };
 
-      }
-
-      let mouseHover = function () {
-        console.log("hover")
-      }
+      let mouseHover = function() {
+        console.log("hover");
+      };
 
       // add in the links
       var link = svg
@@ -167,11 +90,13 @@ buildSankeyTree = (data, phaseData) => {
         .append("path")
         .attr("class", "link")
         .attr("d", sankey.link())
-        .attr("phase", function (d) { return d.target.name })
+        .attr("phase", function(d) {
+          return d.target.name;
+        })
         .on("click", mouseClick)
         .on("mouseover", mouseHover)
-        .style('fill', 'none')
-        .style("stroke-width", function (d) {
+        .style("fill", "none")
+        .style("stroke-width", function(d) {
           return Math.max(1, d.dy);
         });
 
@@ -183,57 +108,69 @@ buildSankeyTree = (data, phaseData) => {
         .enter()
         .append("g")
         .attr("class", "node")
-        .attr("transform", function (d) {
+        .attr("transform", function(d) {
           if (d["value"] > 0) {
             return "translate(" + d.x + "," + d.y + ")";
           }
-        })
-        .call(
-          d3
-            .drag()
-            .subject(function (d) {
-              return d;
-            })
-            .on("start", function () {
-              this.parentNode.appendChild(this);
-            })
-            .on("drag", dragmove)
-        );
+        });
 
+      const nodesToIgnore = [
+        "Expanded Access",
+        "Observational",
+        "Interventional"
+      ];
       // add the rectangles for the nodes
       node
         .append("rect")
-        .attr("height", function (d) {
+        .attr("height", function(d) {
           return d.dy;
         })
         .attr("width", sankey.nodeWidth())
-        .style("fill", function (d) {
+        .style("fill", function(d) {
           return (d.color = color(d.name.replace(/ .*/, "")));
         })
-        .style("stroke", function (d) {
+        .style("stroke", function(d) {
           return d3.rgb(d.color).darker(2);
         })
-        .on("click", function () {
-          return console.log("clicked");
+        .style("cursor", "pointer")
+        .on("click", d => {
+          onNodeClick(d).then(res => createPhaseList(res));
         })
+
         // Add hover text
         .append("title")
-        .text(function (d) {
-          return d.name + "\n" + "There are " + d.value + " studies in this node";
+        .text(function(d) {
+          return (
+            d.name + "\n" + "There are " + d.value + " studies in this node"
+          );
         });
+
+      async function onNodeClick(d) {
+        if (!nodesToIgnore.includes(d["name"]) && d["node"] !== 0) {
+          let phaseLists = [];
+          return new Promise(resolve => {
+            Object.keys(phaseData).forEach(elem => {
+              if (elem.includes(d["name"])) {
+                phaseLists.push(...phaseData[elem]);
+              }
+            });
+            resolve(phaseLists);
+          });
+        }
+      }
 
       // add in the title for the nodes
       let rootVal = data["nodes"][0]["value"];
       node
         .append("text")
         .attr("x", -6)
-        .attr("y", function (d) {
+        .attr("y", function(d) {
           return d.dy / 2;
         })
         .attr("dy", ".35em")
         .attr("text-anchor", "end")
         .attr("transform", null)
-        .text(function (d) {
+        .text(function(d) {
           if (d.value !== 0) {
             if (d.node !== 0) {
               let percentage = Math.round((d.value / rootVal) * 100);
@@ -247,16 +184,15 @@ buildSankeyTree = (data, phaseData) => {
             }
           }
         })
-        .filter(function (d) {
+        .filter(function(d) {
           return d.x < width / 2;
         })
         .attr("x", 6 + sankey.nodeWidth())
         .attr("text-anchor", "start");
 
       var defs = svg.append("defs");
-      link.style('stroke', (d, i) => {
-
-        // make unique gradient ids  
+      link.style("stroke", (d, i) => {
+        // make unique gradient ids
         const gradientID = `gradient${i}`;
         const startColor = d.source.color;
         const stopColor = d.target.color;
@@ -265,35 +201,34 @@ buildSankeyTree = (data, phaseData) => {
         const x2 = d.target.x;
         const y2 = d.target.y;
 
-        const linearGradient = defs.append('linearGradient')
+        const linearGradient = defs
+          .append("linearGradient")
           .attr("gradientUnits", "userSpaceOnUse")
-          .attr('id', gradientID);
+          .attr("id", gradientID);
 
-        linearGradient.attr("x1", x1)
+        linearGradient
+          .attr("x1", x1)
           .attr("y1", y1)
           .attr("x2", x2)
           .attr("y2", y2);
 
-        linearGradient.selectAll('stop')
+        linearGradient
+          .selectAll("stop")
           .data([
-            { offset: '10%', color: startColor },
-            { offset: '90%', color: stopColor }
+            { offset: "10%", color: startColor },
+            { offset: "90%", color: stopColor }
           ])
-          .enter().append('stop')
-          .attr('offset', d => { return d.offset; })
-          .attr('stop-color', d => { return d.color; });
+          .enter()
+          .append("stop")
+          .attr("offset", d => {
+            return d.offset;
+          })
+          .attr("stop-color", d => {
+            return d.color;
+          });
 
         return `url(#${gradientID})`;
-      })
-
-
-      // the function for moving the nodes
-      function dragmove(d) {
-        d3.select(this).attr("transform", "translate(" + d.x + "," + (d.y = Math.max(0, Math.min(height - d.dy, d3.event.y))) + ")");
-        sankey.relayout();
-        link.attr("d", sankey.link());
-      }
-
+      });
     }),
     Promise.resolve()
   );
