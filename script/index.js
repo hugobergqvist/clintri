5d37e3e269a3d26d2c6f9709aa9f30ac7f2fed45
davidmagnussson <davidmagnusson97@gmail.com>
2020-03-05 12:00:05 +0100

    Ändrade stylingen lite på labels i treemap

--- script/index.js
+++ script/index.js
@@ -41,7 +41,7 @@ stateHandler = state => {
 
 //Load data
 loadJSON = () => {
-  return new Promise(function(resolve, reject) {
+  return new Promise(function (resolve, reject) {
     modifyIncomingData().then(data => {
       resolve(data);
     });
@@ -108,11 +108,11 @@ addTreeMap = data => {
 buildTreeMap = data => {
   // set the dimensions and margins of the graph
   var margin = {
-      top: 0,
-      right: 0,
-      bottom: 0,
-      left: 0
-    },
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0
+  },
     //width = 1600 - margin.left - margin.right,
     width = window.innerWidth - 40;
   //height = 1000 - margin.top - margin.bottom;
@@ -128,7 +128,7 @@ buildTreeMap = data => {
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
   // Give the data to this cluster layout:
-  var root = d3.hierarchy(data).sum(function(d) {
+  var root = d3.hierarchy(data).sum(function (d) {
     return d.value;
   }); // Here the size of each leave is given in the 'value' field in input data
 
@@ -139,7 +139,7 @@ buildTreeMap = data => {
     .padding(2)(root);
 
   //Mouseover transitions
-  let mouseOver = function(d) {
+  let mouseOver = function (d) {
     d3.selectAll(".leaf")
       .transition()
       .duration(200)
@@ -150,7 +150,7 @@ buildTreeMap = data => {
       .style("opacity", 1);
   };
 
-  let mouseLeave = function(d) {
+  let mouseLeave = function (d) {
     d3.selectAll(".leaf")
       .transition()
       .duration(200)
@@ -162,7 +162,7 @@ buildTreeMap = data => {
   };
 
   // Tooltip
-  let mousemove = function(d) {
+  let mousemove = function (d) {
     var xPosition = d3.event.pageX + 5;
     var yPosition = d3.event.pageY + 5;
     d3.select("#tooltip")
@@ -172,12 +172,12 @@ buildTreeMap = data => {
     d3.select("#tooltip #value").text("Number of studies: " + d.data.value);
     d3.select("#tooltip").classed("hidden", false);
   };
-  let mouseout = function(d) {
+  let mouseout = function (d) {
     d3.select("#tooltip").classed("hidden", true);
   };
 
   // Move to SankeyTree
-  let mouseClick = function(d) {
+  let mouseClick = function (d) {
     d3.select(this)
       .transition()
       .duration(70)
@@ -201,23 +201,23 @@ buildTreeMap = data => {
     .data(root.leaves())
     .enter()
     .append("rect")
-    .attr("x", function(d) {
+    .attr("x", function (d) {
       return d.x0;
     })
-    .attr("y", function(d) {
+    .attr("y", function (d) {
       return d.y0;
     })
-    .attr("width", function(d) {
+    .attr("width", function (d) {
       return d.x1 - d.x0;
     })
-    .attr("height", function(d) {
+    .attr("height", function (d) {
       return d.y1 - d.y0;
     })
     .attr("class", "leaf")
-    .attr("name", function(d) {
+    .attr("name", function (d) {
       return d.data.name;
     })
-    .style("fill", function() {
+    .style("fill", function () {
       const colorScale = [
         "#EC407B",
         "#D34747",
@@ -266,19 +266,19 @@ buildTreeMap = data => {
     .append("text")
     .attr("x", d => d.x0 + 5)
     .attr("y", d => d.y0 + 20)
-    .attr("transform", function(d) {
+    .attr("transform", function (d) {
       var rect_width = Math.round(d.x1 - d.x0);
-      if (rect_width < 60) {
+      if (rect_width < 70) {
         hasRotated = true;
-        previousX = d.x0;
-        previousY = d.y0 + 30;
+        previousX = d.x0 + 10;
+        previousY = d.y0 + 20;
         return "rotate(" + 90 + "," + previousX + "," + previousY + ")";
       }
     });
 
   text
     .append("tspan")
-    .text(function(d) {
+    .text(function (d) {
       var rect_width = Math.round(d.x1 - d.x0);
       var rect_height = Math.round(d.y1 - d.y0);
       var string = d.data.name;
@@ -292,7 +292,7 @@ buildTreeMap = data => {
         }
       }
 
-      if (rect_width < 60) {
+      if (rect_width < 70) {
         var rect_width = Math.round(d.x1 - d.x0);
         if (string.length * 10 > rect_height) {
           string = string.substring(0, rect_height / 15);
@@ -305,7 +305,7 @@ buildTreeMap = data => {
 
       return string;
     })
-    .attr("font-size", function(d) {
+    .attr("font-size", function (d) {
       var height = Math.round(d.y1 - d.y0);
       if (height < 20) {
         return "0px";
@@ -317,10 +317,10 @@ buildTreeMap = data => {
 
   text
     .append("tspan")
-    .text(function(d) {
+    .text(function (d) {
       var rect_width = Math.round(d.x1 - d.x0);
       var string = "";
-      if (rect_width >= 60) {
+      if (rect_width > 70) {
         var string = "(" + d.data.value + ")";
         var rect_width = Math.round(d.x1 - d.x0);
         if (string.length * 10 > rect_width) {
@@ -329,7 +329,7 @@ buildTreeMap = data => {
         return string;
       }
     })
-    .attr("font-size", function(d) {
+    .attr("font-size", function (d) {
       var height = Math.round(d.y1 - d.y0);
       if (height < 40) {
         return "0px";
@@ -370,7 +370,7 @@ getMaxMinLeafValues = rootData => {
 };
 
 main = () => {
-  loadJSON().then(function(JSON_data) {
+  loadJSON().then(function (JSON_data) {
     addTreeMap(JSON_data);
   });
   /*loadSankeytree().then(function(data) {
